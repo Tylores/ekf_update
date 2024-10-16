a2e8745588e283e1e4b4b9c9b2814d59560c1dda
Tylores <tylor.slay@gmail.com>
2024-10-16 08:12:03 -0700

    still playing around with data structures, but now I should be able to walk through the sparse matrix creation to see where singularity is happening

--- state_estimator/src/main.cpp
+++ state_estimator/src/main.cpp
@@ -3,9 +3,11 @@
 #include <interface/interface.hpp>
 #include <iostream>
 #include <ranges>
+#include <set>
 #include <stdio.h>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #ifdef DEBUG
 #define LOG(message) std::cout << message << std::endl;
@@ -23,48 +25,103 @@ using SCMAP = std::unordered_map<std::string, std::complex<double>>;
 using ICMAP = std::unordered_map<size_t, std::complex<double>>;
 using SDMAP = std::unordered_map<std::string, double>;
 using IDMAP = std::unordered_map<size_t, double>;
+using CVEC = std::vector<std::complex<double>>;
+using CMAT = std::vector<CVEC>;
 
 struct Info {
+  json topology;
   SIMAP index_lookup;
   ISMAP name_lookup;
-  SDMAP base_voltages;
+  SCMAP base_voltages;
   SCMAP base_powers;
   SCMAP voltages;
   SCMAP powers;
 };
 
-SDMAP extractRealInjections(const json &topology) {
-  json obj = topology["injections"]["power_real"];
+CMAT extractAdmittance(const json &topology, const SIMAP &lookup) {
+  json obj = topology["admittance"];
+  json from = obj["from_equipment"];
+  json to = obj["to_equipment"];
+  json values = obj["admittance_list"];
+  std::set<std::string> nodes(from);
+
+  CMAT mat(nodes.size(), CVEC(nodes.size()));
+  for (const auto [src, dst, value] : std::views::zip(from, to, values)) {
+    int i, j = 0;
+    if (lookup.contains(src))
+      i = lookup.at(src);
+    if (lookup.contains(dst))
+      j = lookup.at(dst);
+    if (i != 0 and j != 0)
+      mat[i][j] = std::complex<double>(value[0], value[1]);
+  }
+  return mat;
+};
+
+SDMAP extractRealInjections(const json &injections) {
+  json obj = injections["power_real"];
   json ids = obj["ids"];
   json values = obj["values"];
+  json units = obj["units"];
+
+  double scale = 1.0;
+  if (units == "kW")
+    scale = 1000.0;
 
   SDMAP mapping;
   for (const auto [id, value] : std::views::zip(ids, values)) {
-    mapping.insert({id, value});
+    double x = scale * (double)value;
+    if (mapping.contains(id)) {
+      mapping[id] += x;
+    } else {
+      mapping.insert({id, x});
+    }
   }
   return mapping;
 };
 
-SDMAP extractImagInjections(const json &topology) {
-  json obj = topology["injections"]["power_imaginary"];
+SDMAP extractImaginaryInjections(const json &injections) {
+  json obj = injections["power_imaginary"];
   json ids = obj["ids"];
   json values = obj["values"];
+  json units = obj["units"];
+
+  double scale = 1.0;
+  if (units == "kVAR")
+    scale = 1000.0;
 
   SDMAP mapping;
   for (const auto [id, value] : std::views::zip(ids, values)) {
-    mapping.insert({id, value});
+    double x = scale * (double)value;
+    if (mapping.contains(id)) {
+      mapping[id] += x;
+    } else {
+      mapping.insert({id, x});
+    }
   }
   return mapping;
 };
 
-SDMAP extractBaseVoltages(const json &topology) {
+SDMAP extractBaseVoltagesMagnitudes(const json &topology) {
   json obj = topology["base_voltage_magnitudes"];
   json ids = obj["ids"];
   json values = obj["values"];
 
   SDMAP mapping;
   for (const auto [id, value] : std::views::zip(ids, values)) {
-    mapping.insert({id, value});
+    mapping.insert({id, (double)value});
+  }
+  return mapping;
+};
+
+SDMAP extractBaseVoltagesAngles(const json &topology) {
+  json obj = topology["base_voltage_angles"];
+  json ids = obj["ids"];
+  json values = obj["values"];
+
+  SDMAP mapping;
+  for (const auto [id, value] : std::views::zip(ids, values)) {
+    mapping.insert({id, (double)value});
   }
   return mapping;
 };
@@ -76,12 +133,11 @@ SIMAP extractNodes(const json &topology) {
   SIMAP mapping;
   for (const auto [idx, id] : std::views::enumerate(ids)) {
     mapping.insert({id, idx});
-    LOG(std::format("{} : {}", (std::string)id, idx));
   }
   return mapping;
 };
 
-ISMAP reflect(const SIMAP &lookup) {
+ISMAP reflectMap(const SIMAP &lookup) {
   ISMAP mapping;
   for (const auto &[key, value] : lookup) {
     mapping.insert({value, key});
@@ -89,10 +145,47 @@ ISMAP reflect(const SIMAP &lookup) {
   return mapping;
 }
 
+SCMAP cartMap(const SDMAP &real, const SDMAP &imag) {
+  SCMAP mapping;
+  for (const auto &e : real) {
+    std::string key = e.first;
+    mapping.insert({key, std::complex<double>(real.at(key), imag.at(key))});
+  }
+  return mapping;
+}
+
+SCMAP polarMap(const SDMAP &mag, const SDMAP &ang) {
+  SCMAP mapping;
+  for (const auto &e : mag) {
+    std::string key = e.first;
+    std::complex<double> value = std::polar(mag.at(key), ang.at(key));
+    mapping.insert({key, value});
+  }
+  return mapping;
+}
+
+void printSCMap(const SCMAP &map) {
+  for (const auto &e : map) {
+    LOG(std::format("{} = {},{}", e.first, e.second.real(), e.second.imag()));
+  }
+}
+
 int main(int argc, char *argv[]) {
   std::cout << "State Estimator...\n";
   json topology = loadTopology();
   SIMAP index_lookup = extractNodes(topology);
-  ISMAP name_lookup = reflect(index_lookup);
+  ISMAP name_lookup = reflectMap(index_lookup);
+  LOG(std::format("lookup: {}", index_lookup.size()));
+  SDMAP base_voltage_mag = extractBaseVoltagesMagnitudes(topology);
+  SDMAP base_voltage_ang = extractBaseVoltagesAngles(topology);
+  SCMAP base_voltages = polarMap(base_voltage_mag, base_voltage_ang);
+  LOG(std::format("base_voltages: {}", base_voltages.size()));
+  SDMAP base_power_real = extractRealInjections(topology["injections"]);
+  SDMAP base_power_imag = extractImaginaryInjections(topology["injections"]);
+  SCMAP base_powers = cartMap(base_power_real, base_power_imag);
+  LOG(std::format("powers: {}", base_powers.size()));
+  CMAT admittance = extractAdmittance(topology, index_lookup);
+  LOG(std::format("admittance: {}, {}", admittance.size(),
+                  admittance[0].size()));
   return 0;
 }
